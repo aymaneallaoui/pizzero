a7dac4758889500843e1979d4f4ae1f3a9b8302c
aymaneallaoui <aymanepraxe@gmail.com>
2023-07-01 16:41:22 +0000

    test

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Navbar from './Navbar/navbar';
-import Home from './Home/Home';
-import AboutUs from './About/About';
-import Footer from './Footer/Footer';
-import PizzaMe from './Menu/Menu';
+
+import AboutUs from './components/About/About';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/navbar';
+import Home from './components/Home/Home';
+import PizzaMe from './components/Menu/Menu';
 
 const HomePage = () => {
   return (
