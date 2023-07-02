b690178079076f96ebe9ba45f72306bf997ce175
aymaneallaoui <aymanepraxe@gmail.com>
2023-07-02 20:41:22 +0000

    changes

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react'
 import { Reveal } from '../utils/Reveal'
+import pizza from './pizza.png'
 
 function Home() {
     return (
@@ -8,13 +9,14 @@ function Home() {
             <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80)' }}>
                 {/* <img src="https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" className="max-w-sm rounded-lg shadow-2xl" /> */}
                 <div className="hero-overlay bg-base-100 bg-opacity-60" />
-                <div className="hero-content  text-neutral-content" >
-                    <div className="max-w-md">
+                <div className="hero-content t text-neutral-content lg:flex-row-reverse" >
+                    <div >
+                         <img src={pizza.src} className="max-w-sm rounded-lg shadow-2xl " />
                         <Reveal>
-                        <h1 className="mb-5 text-5xl font-bold [&::selection]:bg-neutral-focus">Hello there</h1>
+                        <h1 className="text-5xl font-bold [&::selection]:bg-neutral-focus">Hello there</h1>
                         </Reveal>
                         <Reveal>
-                        <p className="mb-5 [&::selection]:bg-neutral-focus">
+                        <p className="py-6 [&::selection]:bg-neutral-focus">
                             Welcome to our pizza restaurant! Discover a world of flavorful pizza creations crafted with care and passion. 
                             From traditional Margherita to bold and inventive combinations, our menu offers a delightful range of options to satisfy your cravings.
                              Join us today and let our pizzas transport you to a place of pure culinary bliss.
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
   plugins: [require('tailwind-scrollbar-daisyui'),
     require("daisyui")],
    daisyui: {
-    themes: ["luxury" ], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    themes: ["luxury"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "luxury", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
